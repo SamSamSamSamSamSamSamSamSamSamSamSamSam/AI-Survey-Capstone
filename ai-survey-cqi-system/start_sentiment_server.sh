4f090a6b99db2fb2d0983591989e7e90b04be2c6
Juju <133754317+ArjoyEstiola@users.noreply.github.com>
2026-04-02 19:34:03 +0800

    Add: added a bash file for running the Distilbert model in the server

---
+++ ai-survey-cqi-system/start_sentiment_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting DistilBERT Sentiment Server..."
+# Linux uses forward slashes and usually 'bin' instead of 'Scripts'
+./resources/python/myenv/bin/python resources/python/sentiment_server.py
