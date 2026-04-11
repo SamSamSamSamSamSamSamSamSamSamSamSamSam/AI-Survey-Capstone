c8fe4307d14e69282d8b3951214c085e1f6713f6
SamPacs <20101786@usc.edu.ph>
2026-04-11 12:03:25 +0800

    fix: user model error fix

--- ai-survey-cqi-system/app/Models/User.php
+++ ai-survey-cqi-system/app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 
